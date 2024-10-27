fea4872e2aac0b9800d3571d8b371dd03615ec7e
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2024-10-27 13:00:50 +0400

    feat: rephrase the footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="flex justify-center">
-    <p class="flex gap-x-1">
-      Built by
+    <p class="flex gap-x-1 text-sm md:text-base">
+      © 2024 - {{ currentYear }} ·
       <a
         class="underline font-semibold"
         href="https://github.com/derecklhw/"
         target="_blank"
-        >derecklhw</a
       >
-      with
-      <IconsHeartSolid class="size-6" />
+        Dereck Lam Hon Wah
+      </a>
+      · Mauritius 🇲🇺
     </p>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+const currentYear = ref<number>(2024);
+
+onMounted(async () => {
+  currentYear.value = new Date().getFullYear();
+});
+</script>
